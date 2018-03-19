007a58c8bc07e08d297148ab38819b2d54d65d58
romainbezard <romain.bezard@hotmail.com>
2018-03-19 10:31:10 +0100

    Ajout page login

---
+++ app/src/main/java/com/exemple/gsb_android/activity_main.java
@@ -0,0 +1,17 @@
+package com.exemple.gsb_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by romain.bezard on 19/03/2018.
+ */
+
+public class activity_main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_main);
+    }
+}
