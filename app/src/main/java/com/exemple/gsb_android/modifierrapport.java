6d472cc09719058c2cc31131dda4c5f7cd009b3f
celinedls <celine.duclos01@gmail.com>
2018-03-23 09:03:32 +0100

    ModifierRapport création

---
+++ app/src/main/java/com/exemple/gsb_android/modifierrapport.java
@@ -0,0 +1,23 @@
+package com.exemple.gsb_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by celine.duclos on 23/03/2018.
+ */
+
+public class modifierrapport extends Activity {
+    
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.modifierrapport);
+        imageView = (ImageView)findViewById(R.id.imageGSB);
+        imageView.setImageResource(R.mipmap.gsb);
+    }
+}
