bfd79ae4ae2cac6bbb770e981feef8133213e81b
charles.morel <charles-antony@live.fr>
2018-03-23 11:56:33 +0100

    Class Visiteur /Rapport

--- app/src/main/java/com/exemple/gsb_android/classVisiteur.java
+++ app/src/main/java/com/exemple/gsb_android/classVisiteur.java
@@ -1,5 +1,7 @@
 package com.exemple.gsb_android;
 
+import java.net.IDN;
+
 /**
  * Created by charles.morel on 23/03/2018.
  */
@@ -15,15 +17,63 @@ public class classVisiteur {
     private String ville;
     private char dateEmbauche;
 
-    public void classVisiteur(String ID, String Nom, String Prenom, String Mdp, String Adresse, String Cp, String Ville, char DateEmbauche)
+    public classVisiteur(String ID, String Nom, String Prenom,String Login, String Mdp, String Adresse, String Cp, String Ville, char DateEmbauche)
     {
         id = ID;
         nom = Nom;
         prenom = Prenom;
+        login = Login;
         mdp = Mdp;
         adresse = Adresse;
         cp = Cp;
         ville = Ville;
         dateEmbauche = DateEmbauche;
+    }
+
+    // Set
+
+    public void setNom(String ID){
+        id = ID;
+    }
+
+    public void setPrenom(String Prenom){
+        prenom = Prenom;
+    }
+
+    public void setAdresse(String Adresse){
+        adresse = Adresse;
+    }
+
+    public void setCp(String CP){
+        cp = CP;
+    }
+
+    public void setVille(String Ville){
+        ville= Ville;
+    }
+
+    // Get
+    public String getId(){
+        return id;
+    }
+
+    public String getNom(){
+        return nom;
+    }
+
+    public String getPrenom(){
+        return prenom;
+    }
+
+    public String getAdresse(){
+        return adresse;
+    }
+
+    public String getVille(){
+        return ville;
+    }
+
+    public String getCp(){
+        return cp;
     }
 }
