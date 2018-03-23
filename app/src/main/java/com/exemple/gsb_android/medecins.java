19bdfa695b8b95bd0d9022654c347ae19edb8ffa
celinedls <celine.duclos01@gmail.com>
2018-03-23 10:53:02 +0100

    AjouterRapport création Médecin création

---
+++ app/src/main/java/com/exemple/gsb_android/medecins.java
@@ -0,0 +1,23 @@
+package com.exemple.gsb_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+/**
+ * Created by celine.duclos on 23/03/2018.
+ */
+
+public class medecins extends Activity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.medecins);
+        imageView = (ImageView)findViewById(R.id.imageGSB);
+        imageView.setImageResource(R.mipmap.gsb);
+    }
+}
