b0cbaa9895df5d2d8c028a39b497461dbc19de52
romainbezard <romain.bezard@hotmail.com>
2018-03-23 08:17:32 +0100

    Modification fonction accueil

--- app/src/main/java/com/exemple/gsb_android/activity_main.java
+++ app/src/main/java/com/exemple/gsb_android/activity_main.java
@@ -1,14 +1,10 @@
 package com.exemple.gsb_android;
 
 import android.app.Activity;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-
-import com.exemple.gsbrapports.LocalSQLiteOpenHelper;
 
 /**
  * Created by romain.bezard on 19/03/2018.
@@ -24,9 +20,21 @@ public class activity_main extends Activity {
     protected void onCreate(Bundle bundle){
         super.onCreate(bundle);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView)findViewById(R.id.imageGSB);
+
+        // Instanciation de l'image
+        imageView = findViewById(R.id.imageGSB);
         imageView.setImageResource(R.mipmap.gsb);
+
+        // Instanciation des textBox
+        login = findViewById(R.id.Login);
+        mdp = findViewById(R.id.Mdp);
+
+        // Instanciation du bouton connexion
+        btnConnexion = findViewById(R.id.Btn_connexion);
+
     }
+
+    // Quand on clique sur le bouton de connexion
 
     private void btnConnexion_Click(){
 
